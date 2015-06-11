d276af8d0dcf887d5a872ad1f035c5b2f16a05cf
Sumesh <Sumesh@kiranP-Mac--Pro-4.local>
2015-06-11 14:07:35 +0530

    basic tabor created

--- MyRoppingApp/CollectionViewController.m
+++ MyRoppingApp/CollectionViewController.m
@@ -152,7 +152,12 @@ static NSString * const reuseIdentifier3 = @"CellType3";
 }
 
 #pragma mark <UICollectionViewDelegate>
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item == 0) {
+        [self performSegueWithIdentifier:@"Cell1Segue" sender:self];
 
+    }
+}
 /*
 // Uncomment this method to specify if the specified item should be highlighted during tracking
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
